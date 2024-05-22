dbceea938c7c1ac058a10c1e875011c429f6ece0
César Oliveira <cesaroliiveira8@gmail.com>
2024-05-22 20:22:42 -0300

    Atualizando

--- front-end/app.py
+++ front-end/app.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 import requests
 
@@ -15,21 +16,51 @@ class Agendamento():
         self.frames_da_tela()
         self.tela()
         self.botoes()
+        self.labelsAndEntrys()
+
 
     def tela(self):
         self.root.title("Agendamento")
         self.root.geometry("1440x1020")
     def frames_da_tela(self):
         self.frame_1 = Frame(self.root)
-        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.5)
+        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.95)
+
+        self.frame_2 = Frame(self.frame_1)
+        self.frame_2.place(relx=0.25, rely=0.25, relwidth=0.7, relheight=0.6)
+        self.frame_2.configure(bg="red")
 
     def botoes(self):
         # Botão de Voltar
         self.bt_voltar = Button(self.frame_1, text="Voltar", command=lambda: Page(Menu))
-        self.bt_voltar.place(relx=0.3, rely=0.2, relwidth=0.1, relheight=0.15)
+        self.bt_voltar.place(relx=0.01, rely=0.02, width=50, height=35)
 
-        ##Botão de Cadastro de Agenda
-        self.bt_agendamento = Button(self.frame_1, text="Agendamento")
+
+    def labelsAndEntrys(self):
+
+        options = [
+            "Análise e Desenvolvimento de Sistemas",
+            "Psicologia",
+            "Direito",
+            "Financeiro"
+        ]
+        clicked = StringVar()
+
+        clicked.set("Curso")
+
+        ##Entry nome do professor
+        ## Entrada do ID
+        self.ent_professor = Entry(self.frame_2)
+
+        self.ent_professor.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+
+        self.drop_disciplina = OptionMenu(self.frame_2, clicked, *options)
+        self.drop_disciplina.grid(row=1, column=0)
+
+
+
+
+
 
 
 class Menu():
@@ -50,8 +81,8 @@ class Menu():
     def botoes(self):
 
         ##Botão de Cadastro de Agenda
-        self.bt_agendamento = Button(self.frame_1, text="Agendamento", command=Agendamento)
-        self.bt_agendamento.place(relx=0.3, rely=0.2, relwidth=0.1, relheight=0.15)
+        self.bt_agendamento = Button(self.frame_1, text="Agendamento", command=lambda: {self.frame_1.destroy(), Page(Agendamento)})
+        self.bt_agendamento.place(relx=0.1, rely=0.2, relwidth=0.1, relheight=0.15)
         self.bt_agendamento.grid(row=0, column=0)
 
         ##Botão de Cadastro de Agenda
@@ -111,13 +142,11 @@ class Application():
 
             bdUser = request_json['Authenticate']['code']
 
-
-
             if bdUser == 0:
                 messagebox.showerror(title="Login", message="Usuario ou Senha incorretos.")
             elif bdUser == 1:
                 messagebox.showinfo(title="Login", message="Logado com Sucesso.")
-                self.frame_1.destroy()
+
                 Page(Menu)
             else:
                 messagebox.showwarning(title="Login", message="Algo de Errado aconteceu.")
@@ -147,6 +176,7 @@ class Application():
         self.ent_senha = Entry(self.frame_1, show="*")
         self.ent_senha.place(relx=0.05, rely=0.55, relwidth=0.25, height=35)
         self.lb_id.grid(row=0, column=3)
+
 
 
 def Page(page):
