944899bc8cb6172b64eb3b02a8148c611a8312b9
César Oliveira <cesaroliiveira8@gmail.com>
2024-06-04 09:39:28 -0300

    Colocando agendamento e consulta

--- back-end/app.py
+++ back-end/app.py
@@ -12,7 +12,7 @@ db = SQLAlchemy(app)
 
 
 class usuario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(255))
     email = db.Column(db.String(255))
     senha = db.Column(db.String(255))
@@ -23,7 +23,21 @@ class usuario(db.Model):
     def aut_response(self, code):
         return {"id": self.id, "nome": self.nome, "code": code}
 
+class agendamento(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    professor = db.Column(db.String(255))
+    materia = db.Column(db.String(255))
+    laboratorio = db.Column(db.String(255))
+    data = db.Column(db.String(255))
 
+    def to_json(self):
+        return {"id": self.id, "professor": self.professor, "materia": self.materia, "laboratorio": self.laboratorio, "data": self.data}
+
+    def response(self, code):
+        return {"id": self.id, "professor": self.professor, "materia": self.materia, "laboratorio": self.laboratorio, "data": self.data, "code": code}
+
+with app.app_context():
+    db.create_all()
 
 
 #Selecionar todos os usuarios
@@ -58,13 +72,28 @@ def authenticUser():
         content = {"code": 0}
         return gera_response(404, "Authenticate", content, "Usuario ou Senha não encontrado.")
 
+@app.route("/agendamento", methods=['POST'])
+def agendar():
+    body = request.get_json()
 
+    try:
+        bodyRequest = agendamento(professor=body["professor"], materia=body["materia"],
+                                  laboratorio=body["laboratorio"], data=body["data"])
+        db.session.add(bodyRequest)
+        db.session.commit()
+        return gera_response(201, "Agendamento", bodyRequest.response(code=1), "Agendamento Cadastrado.")
+    except Exception as e:
+        print(e)
+        return gera_response(400, "", {}, "Erro ao cadastrar")
 
+@app.route("/agendamento", methods=['GET'])
+def pegarAgendamento():
+    getAgendamento = agendamento.query.all()
+    getAgendamento_json = [agendamento.to_json() for agendamento in getAgendamento]
 
-
+    return gera_response(200, "Agendamento", getAgendamento_json)
 
 ##DTO
-
 def gera_response(status, nome_do_conteudo, conteudo, mensagem=False):
     body = {}
     body[nome_do_conteudo] = conteudo
--- front-end/app.py
+++ front-end/app.py
@@ -1,11 +1,69 @@
 from tkinter import *
 import tkinter as tk
 from tkinter import messagebox
-import requests
+from tkinter import ttk
+from menu import Menu
 
+import requests
 
 root = Tk()
 
+
+
+
+class Consulta():
+    def __init__(self):
+        self.root = root
+        self.frames_da_tela()
+        self.tela()
+        self.botoes()
+        self.lista_frame()
+
+        self.listaCli.delete(*self.listaCli.get_children())
+
+        link = 'http://127.0.0.1:5000/agendamento'
+
+        requestResponse = requests.get(link)
+        lista = requestResponse.json()
+
+        resultado = lista['Agendamento']
+
+        for i in resultado:
+            self.listaCli.insert("", END, values=(i['id'], i['professor'], i['materia'], i['laboratorio'], i['data']))
+
+
+    def tela(self):
+        self.root.geometry("1440x1020")
+    def frames_da_tela(self):
+        self.frame_1 = Frame(self.root)
+        self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.95)
+
+    def lista_frame(self):
+        self.listaCli = ttk.Treeview(self.frame_1, height=3, column=("col1", "col2", "col3", "col4", "col5"))
+        self.listaCli.heading("#0", text="")
+        self.listaCli.heading("#1", text="ID")
+        self.listaCli.heading("#2", text="Professor")
+        self.listaCli.heading("#3", text="Disciplina")
+        self.listaCli.heading("#4", text="Laboratorio")
+        self.listaCli.heading("#5", text="Data")
+
+        self.listaCli.column("#0", width=1)
+        self.listaCli.column("#1", width=2)
+        self.listaCli.column("#2", width=200)
+        self.listaCli.column("#3", width=125)
+        self.listaCli.column("#4", width=125)
+        self.listaCli.column("#5", width=125)
+
+        self.scroolLista = Scrollbar(self.frame_1, orient='vertical')
+        self.listaCli.configure(yscroll=self.scroolLista.set)
+        self.scroolLista.place(relx=0.96, rely=0.1, relwidth=0.04, relheight=0.85)
+
+        self.listaCli.place(relx=0.01, rely=0.1, relwidth=0.95, relheight=0.85)
+
+    def botoes(self):
+        # Botão de Voltar
+        self.bt_voltar = Button(self.frame_1, text="Voltar", command=lambda: {self.frame_1.destroy(), Page(Menu)})
+        self.bt_voltar.place(relx=0.01, rely=0.02, width=50, height=35)
 
 
 
@@ -18,48 +76,125 @@ class Agendamento():
         self.botoes()
         self.labelsAndEntrys()
 
-
     def tela(self):
-        self.root.title("Agendamento")
         self.root.geometry("1440x1020")
     def frames_da_tela(self):
         self.frame_1 = Frame(self.root)
         self.frame_1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.95)
 
-        self.frame_2 = Frame(self.frame_1)
-        self.frame_2.place(relx=0.25, rely=0.25, relwidth=0.7, relheight=0.6)
-        self.frame_2.configure(bg="red")
-
     def botoes(self):
         # Botão de Voltar
-        self.bt_voltar = Button(self.frame_1, text="Voltar", command=lambda: Page(Menu))
+        self.bt_voltar = Button(self.frame_1, text="Voltar", command=lambda: {self.frame_1.destroy(), Page(Menu)})
         self.bt_voltar.place(relx=0.01, rely=0.02, width=50, height=35)
 
 
-    def labelsAndEntrys(self):
 
-        options = [
+
+    def labelsAndEntrys(self):
+        def professor_focus_in(event):
+            if self.ent_professor.get() == "Professor":
+                self.ent_professor.delete(0, 'end')
+                self.ent_professor.config(fg="Black")
+
+        def professor_focus_out(event):
+            if self.ent_professor.get() == "":
+                self.ent_professor.insert(0, "Professor")
+
+        def data_focus_in(event):
+            if self.ent_data.get() == "00/00/00":
+                self.ent_data.delete(0, 'end')
+                self.ent_data.config(fg="Black")
+
+        def data_focus_out(event):
+            if self.ent_data.get() == "":
+                self.ent_data.insert(0, "00/00/00")
+
+
+        disciplinas = [
             "Análise e Desenvolvimento de Sistemas",
             "Psicologia",
             "Direito",
             "Financeiro"
         ]
-        clicked = StringVar()
 
-        clicked.set("Curso")
+        laboratorios = [
+            "Laboratório 1",
+            "Laboratório 2",
+            "Laboratório Quimica",
+            "Sala 12"
+        ]
+
+        clickedDisciplinas = StringVar()
+        clickedDisciplinas.set("Curso")
+
+        clickedLaboratorios = StringVar()
+        clickedLaboratorios.set("Laboratorios")
 
         ##Entry nome do professor
         ## Entrada do ID
-        self.ent_professor = Entry(self.frame_2)
+        self.ent_professor = Entry(self.frame_1, font=("Helvetica", 13), fg="Gray")
+
 
         self.ent_professor.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+        self.ent_professor.place(relx=0.1, rely=0.15, relwidth=0.2, height=40)
+        self.ent_professor.insert(0, "Professor")
+        self.ent_professor.bind("<FocusIn>", professor_focus_in)
+        self.ent_professor.bind("<FocusOut>", professor_focus_out)
 
-        self.drop_disciplina = OptionMenu(self.frame_2, clicked, *options)
-        self.drop_disciplina.grid(row=1, column=0)
+
+
+        ## Disciplinas
+        self.drop_disciplina = OptionMenu(self.frame_1, clickedDisciplinas, *disciplinas)
+        self.drop_disciplina.place(relx=0.4, rely=0.15, relwidth=0.2, height=50)
+
+        ## Laboratorios
+        self.drop_laboratorios = OptionMenu(self.frame_1, clickedLaboratorios, *laboratorios)
+        self.drop_laboratorios.place(relx=0.1, rely=0.23, relwidth=0.2, height=50)
+
+        self.ent_data = Entry(self.frame_1, font=("Helvetica", 13), fg="Gray")
+        self.ent_data.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+        self.ent_data.place(relx=0.4, rely=0.23, relwidth=0.2, height=40)
+        self.ent_data.insert(0, "00/00/00")
+        self.ent_data.bind("<FocusIn>", data_focus_in)
+        self.ent_data.bind("<FocusOut>", data_focus_out)
+
+        def agendardia():
+            professor = self.ent_professor.get()
+            disciplina = clickedDisciplinas.get()
+            laboratorio = clickedLaboratorios.get()
+            agendamentodata = self.ent_data.get()
+
+            if professor == '':
+                return messagebox.showerror(title="Agendamento", message="Professor não pode estar vázio!")
+            elif disciplina == '':
+                return messagebox.showerror(title="Agendamento", message="Disciplina não pode estar vázio!")
+            elif laboratorio == '':
+                return messagebox.showerror(title="Agendamento", message="Laboratorio não pode estar vázio!")
+            elif agendamentodata == '':
+                return messagebox.showerror(title="Agendamento", message="Data não pode estar vázio!")
+
+
+            data = {"professor": professor, "materia": disciplina, "laboratorio": laboratorio, "data": agendamentodata}
+
+            link = 'http://127.0.0.1:5000/agendamento'
+
+            requestResponse = requests.post(link, json=data)
+            request_json = requestResponse.json()
+
+            code = request_json['Agendamento']['code']
+
+            mensagem = request_json['mensagem']
+
+            if code == 1:
+                messagebox.showinfo(title="Agendamento", message=mensagem)
+            else:
+                messagebox.showerror(title="Agendamento", message="Algo de errado aconteceu.")
 
 
 
-
+        # Botão Enviar
+        self.bt_ok = Button(self.frame_1, text="Enviar", command=lambda: agendardia())
+        self.bt_ok.place(relx=0.55, rely=0.5, width=70, height=40)
 
 
 
@@ -71,7 +206,6 @@ class Menu():
         self.botoes()
 
     def tela(self):
-        self.root.title("Menu")
         self.root.geometry("1440x1020")
     def frames_da_tela(self):
         self.frame_1 = Frame(self.root)
@@ -82,13 +216,13 @@ class Menu():
 
         ##Botão de Cadastro de Agenda
         self.bt_agendamento = Button(self.frame_1, text="Agendamento", command=lambda: {self.frame_1.destroy(), Page(Agendamento)})
-        self.bt_agendamento.place(relx=0.1, rely=0.2, relwidth=0.1, relheight=0.15)
-        self.bt_agendamento.grid(row=0, column=0)
+        self.bt_agendamento.place(relx=0.35, rely=0.4, relwidth=0.1, relheight=0.15)
+
 
         ##Botão de Cadastro de Agenda
-        self.bt_consulta = Button(self.frame_1, text="Consulta")
-        self.bt_consulta.place(relx=0.3, rely=0.2, relwidth=0.1, relheight=0.15)
-        self.bt_consulta.grid(row=0, column=1, padx=20)
+        self.bt_consulta = Button(self.frame_1, text="Consulta", command=lambda: {self.frame_1.destroy(), Page(Consulta)})
+        self.bt_consulta.place(relx=0.55, rely=0.4, relwidth=0.1, relheight=0.15)
+
 
 
 
@@ -104,8 +238,10 @@ class Application():
         self.botoes()
         self.labelsAndEntry()
 
+
+
     def tela(self):
-        self.root.title("Login")
+        self.root.title("LabPlanner")
         self.root.configure(background='#1e3743')
         self.root.geometry("788x500")
         self.root.resizable(True, True)
@@ -137,8 +273,6 @@ class Application():
             requestResponse = requests.post(link, json=data)
             request_json = requestResponse.json()
 
-            print(requestResponse)
-            print(requestResponse.json())
 
             bdUser = request_json['Authenticate']['code']
 
@@ -146,7 +280,6 @@ class Application():
                 messagebox.showerror(title="Login", message="Usuario ou Senha incorretos.")
             elif bdUser == 1:
                 messagebox.showinfo(title="Login", message="Logado com Sucesso.")
-
                 Page(Menu)
             else:
                 messagebox.showwarning(title="Login", message="Algo de Errado aconteceu.")
@@ -154,28 +287,64 @@ class Application():
 
         ##Botão de entrada
         self.bt_ok = Button(self.frame_1, text="Entrar", command=login)
-        self.bt_ok.place(relx=0.3, rely=0.2, width=85, height=35)
+        self.bt_ok.place(relx=0.41, rely=0.65, width=85, height=35)
 
     def labelsAndEntry(self):
+
+        def id_focus_in(event):
+            if self.ent_professor.get() == "Professor":
+                self.ent_id.delete(0, 'end')
+                self.ent_id.config(fg="Black")
+
+        def id_focus_out(event):
+            if self.ent_id.get() == "":
+                self.ent_id.insert(0, "Professor")
+
+        def password_focus_in(event):
+            if self.ent_senha.get() == "00/00/00":
+                self.ent_senha.delete(0, 'end')
+                self.ent_senha.config(fg="Black")
+
+        def password_focus_out(event):
+            if self.ent_senha.get() == "":
+                self.ent_senha.insert(0, "00/00/00")
+
+
+
         ## Label ID
         self.lb_id = Label(self.frame_1, text="ID:")
         self.lb_id.place(relx=0.00, rely=0.05, relwidth=0.25, relheight=0.15)
         self.lb_id.grid(row=0, column=0)
 
         ## Entrada do ID
-        self.ent_id = Entry(self.frame_1)
-        self.ent_id.place(relx=0.05, rely=0.15, relwidth=0.25, height=35)
-        self.lb_id.grid(row=0, column=1)
+        self.ent_id = Entry(self.frame_1, font=("Helvetica", 13), fg="Gray")
+        self.ent_id.place(relx=0.35, rely=0.35, relwidth=0.25, height=35)
+        self.ent_id.insert(0, "ID")
+        self.ent_id.bind("<FocusIn>", id_focus_in)
+        self.ent_id.bind("<FocusOut>", id_focus_out)
+
+        self.ent_professor = Entry(self.frame_1, font=("Helvetica", 13), fg="Gray")
+
+        self.ent_professor.grid(column=0, row=0, sticky=tk.W, padx=5, pady=5)
+        self.ent_professor.place(relx=0.1, rely=0.15, relwidth=0.2, height=40)
+        self.ent_professor.insert(0, "Professor")
+        self.ent_professor.bind("<FocusIn>", professor_focus_in)
+        self.ent_professor.bind("<FocusOut>", professor_focus_out)
+
 
         ## Label da Senha
         self.lb_senha = Label(self.frame_1)
         self.lb_senha.place(relx=0.00, rely=0.4, relwidth=0.25, relheight=0.15)
-        self.lb_id.grid(row=0, column=2)
+
 
         ##Entrada da senha;
         self.ent_senha = Entry(self.frame_1, show="*")
-        self.ent_senha.place(relx=0.05, rely=0.55, relwidth=0.25, height=35)
-        self.lb_id.grid(row=0, column=3)
+        self.ent_senha.place(relx=0.35, rely=0.5, relwidth=0.25, height=35)
+
+
+
+
+
 
 
 
